e5047139c5469d9fa56e0a9c50f8364b3a79e025
achan40 <aricchan918@gmail.com>
2025-03-11 10:44:04 -0700

    change vite.config for amazon ec2

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Expose to all network interfaces
+    port: 3000, // You can change this to any port you want (e.g., 5173)
+  },
+});
